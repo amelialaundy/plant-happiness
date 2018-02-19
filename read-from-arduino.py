f57eec4dff00982d9fbb3bea39150d1822cd6766
amelialaundy <amelialaundy@gmail.com>
2018-02-20 10:02:27 +1300

    add try catch to python script to save file on error

--- read-from-arduino.py
+++ read-from-arduino.py
@@ -1,5 +1,6 @@
 import serial
 import time as time
+import sys
 from datetime import datetime
 from datetime import timedelta 
 ser = serial.Serial('/dev/ttyACM0', 9600)
@@ -9,16 +10,20 @@ file_name = 'plant-reading-{0}.txt'.format(now.strftime('%Y-%m-%d %H:%M:%S'))
 
 file = open(file_name,"w")
 
-future_date = now + timedelta(hours=8)
+future_date = now + timedelta(hours=1)
 
 while (datetime.now() < future_date):
-    line = ser.readline()
-    print(line)
-    formatted_line = line.decode("utf-8")
-    file.write(formatted_line)
-    if (formatted_line.startswith("Soil humidity:")):
-        file.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-        file.write('\n\n')
+    try:
+        line = ser.readline()
+        print(line)
+        formatted_line = line.decode("utf-8")
+        file.write(formatted_line)
+        if (formatted_line.startswith("Soil humidity:")):
+            file.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+            file.write('\n\n')
+    except:
+        print(sys.exc_info()[0])
+        file.close()
         
 
 file.close()
