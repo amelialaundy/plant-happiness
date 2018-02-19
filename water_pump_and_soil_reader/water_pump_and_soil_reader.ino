e9f6ccec785c8578522bc2524b815437285d5315
amelialaundy <amelialaundy@gmail.com>
2018-02-20 10:03:12 +1300

    refactor arduino code, make sure pump only runs for 2 seconds and then turns off, add 20 minute delay to reading soil humidity

--- water_pump_and_soil_reader/water_pump_and_soil_reader.ino
+++ water_pump_and_soil_reader/water_pump_and_soil_reader.ino
@@ -4,6 +4,12 @@ const int hygrometer = A0;  //Hygrometer sensor analog pin output at pin A0 of A
 const int noWaterSpeed = 0;
 const int waterSpeed = 10;
 const bool debug = true;
+const unsigned long default_delay = 1200000; //60000 * 20; //20 minutes
+const int wet = 40;
+const int dry = 1000;
+const int hundred_percent = 100;
+const int zero_percent = 0;
+const unsigned long watering_period = 2000; //2 seconds
 
 void setup() { 
   pinMode(motorPin, OUTPUT);
@@ -15,29 +21,27 @@ void setup() {
 void loop() { 
   int soilHumidity = readSoilHumidityPercentage();
   bool shouldWaterPlant = soilHumidity <= 50;
-  shouldWaterPlant ?  turnPumpOn() : turnPumpOff();
-  delay(2000); //Read every 2 sec.
+  shouldWaterPlant ?  waterPlant() : turnPumpOff();
+  Serial.print(default_delay);
+  delay(default_delay); //Read every 20 mins ater watering or turning off pump
 } 
 
 int readSoilHumidityPercentage() {
   int rawValue = analogRead(hygrometer);   //Read analog value 
-  Serial.print("raw value: ");
-  Serial.print(rawValue);
-  Serial.print("\n");
-  int constrainedValue = constrain(rawValue,40,1000);  //Keep the ranges!
-//    int constrainedValue = constrain(rawValue,400,1023);  //Keep the ranges!
-  Serial.print("constrained value: ");
-  Serial.print(constrainedValue);
-  Serial.print("\n");
-  Serial.print("\n");
-  int humidityPercent =  map(constrainedValue,40,1000,100,0);  //Map value : 400 will be 100 and 1023 will be 0
-// int humidityPercent =  map(constrainedValue,400,1023,100,0);  //Map value : 400 will be 100 and 1023 will be 0
-  logSoilHumidity(humidityPercent);
+  int constrainedValue = constrain(rawValue,wet,dry); //constrain values to within the wettest value and driest value
+  int humidityPercent =  map(constrainedValue, wet, dry, hundred_percent, zero_percent);  //Map value : wet will be 100 and dry will be 0
+  logSoilHumidity(rawValue, constrainedValue, humidityPercent);
   return humidityPercent;
 }
 
-void turnPumpOn() {
+void waterPlant() {
   logWatering();
+  turnPumpOn();
+  delay(watering_period); //0nly water for set period of time
+  turnPumpOff(); // then turn off pump
+}
+
+void turnPumpOn() {
   digitalWrite(motorPin, waterSpeed);
 }
 
@@ -45,7 +49,13 @@ void turnPumpOff() {
   digitalWrite(motorPin, noWaterSpeed);
 }
 
-void logSoilHumidity(int humidity) {
+void logSoilHumidity(int rawValue, int constrainedValue,int humidity) {
+  Serial.print("raw value: ");
+  Serial.print(rawValue);
+  Serial.print("\n");
+  Serial.print("constrained value: ");
+  Serial.print(constrainedValue);
+  Serial.print("\n");
   Serial.print("Soil humidity: ");
   Serial.print(humidity);
   Serial.print("%\n");
@@ -54,7 +64,6 @@ void logSoilHumidity(int humidity) {
 void logWatering() {
     Serial.print("Watering plant at speed:  ");
     Serial.print(waterSpeed);
-    Serial.print("\n");
     Serial.print("\n");
 }
 
